99b221e4ecfff5bd345a3b85a305cb1f8aff780e
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-09-13 17:34:29 +0900

    @Query annotation

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,5 +16,18 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    
+    // @Query 어노테이션안에 JPQL로 작성한 쿼리문을 넣는다.
+    // from 뒤에 엔티티 클래스로 작성한 item을 지정해 주고, item으로부터 데이터를 select하겠다는 것을 의미
+    // @Param 어노테이션을 이용하여 파라미터로 넘어온 값을 JPQL에 들어갈 변수로 지정해줄 수 있음.
+    // @Param 어노테이션 대신 파라미터의 순서를 이용해 전달가능 -> .itemDetail대신 '?1' 사용
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    // native Query
+    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true )
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 
 }
