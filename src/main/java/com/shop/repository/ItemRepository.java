98b54e0c34b079fa498effad590f95c0e176981f
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-09-13 18:30:40 +0900

    Querydsl

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,11 +3,12 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
     List<Item> findByItemNm(String itemNm);
 
@@ -22,7 +23,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     // from 뒤에 엔티티 클래스로 작성한 item을 지정해 주고, item으로부터 데이터를 select하겠다는 것을 의미
     // @Param 어노테이션을 이용하여 파라미터로 넘어온 값을 JPQL에 들어갈 변수로 지정해줄 수 있음.
     // @Param 어노테이션 대신 파라미터의 순서를 이용해 전달가능 -> .itemDetail대신 '?1' 사용
-
     @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
     List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,17 +1,26 @@
 package com.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.input.BomInput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -19,6 +28,10 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    // 영속성 컨텍스트를 사용하기 위해 @PersistenceContext을 이용하여 EntityManager 빈을 주입
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -46,55 +59,145 @@ class ItemRepositoryTest {
             System.out.println(item.toString());
         }
     }
-    
+
     @Test
     @DisplayName("상품명, 상품상세설명 or 테스트")
-    public void findByItemOrItemDetailTest(){
+    public void findByItemOrItemDetailTest() {
         this.createItemList();
-        List<Item> itemList= itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
-        for(Item item : itemList){
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
-    
+
     @Test
     @DisplayName("가격 LessThan 테스트")
-    public void findByPriceLessThanTest(){
+    public void findByPriceLessThanTest() {
         this.createItemList();
         List<Item> itemList = itemRepository.findByPriceLessThan(10005);
-        for(Item item : itemList){
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
 
     @Test
     @DisplayName("가격 내림차순 조회 테스트")
-    public void findByPriceLessThanOrderByPriceDesc(){
+    public void findByPriceLessThanOrderByPriceDesc() {
         this.createItemList();
         List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
-        for(Item item : itemList){
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
-    
+
     @Test
     @DisplayName("@Query를 이용한 상품 조회 테스트")
-    public void findByItemDetailTest(){
+    public void findByItemDetailTest() {
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
-        for(Item item : itemList){
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
-    
+
     @Test
     @DisplayName("native Query 속성을 이용한 상품 조회 테스트")
-    public void findByItemDetailByNative(){
+    public void findByItemDetailByNative() {
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
-        for(Item item : itemList){
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
-
     }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        this.createItemList();
+
+        // JPAQueryFactory를 이용하여 쿼리를 동적으로 생성
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        // Querydsl을 통해 쿼리를 생성하기 위해 플러그인을 통해 자동으로 생성된 Qitem 객체를 이용
+        QItem qItem = QItem.item;
+
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        // 쿼리 결과를 리스트로 반환 fetch() 메소드 실행 시점에 쿼리문이 실행
+        List<Item> itemList = query.fetch();
+
+        // List<T> fetch()               -> 조회 결과 리스트 반환
+        // T fetchOne                    -> 조회 대상이 1건인 경우 제네릭으로 지정한 타입 반환
+        // T fetchFirst                  -> 조회 대상 중 1건만 반환
+        // Long fetchCount()             -> 조회 대상 개수 반환
+        // QueryResult<T> fetchResults() -> 조회한 리스트와 전체 개수를 포함한 QueryResults 반환
+
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    public void createItemList2() {
+        for (int i = 1; i <= 5; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+        for (int i = 6; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNumber(0);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+
+        }
+    }
+
+    @Test
+    @DisplayName("상품 Querydsl 조회 테스트2")
+    public void queryDslTest2(){
+        this.createItemList2();
+
+        // 쿼리에 들어갈 조건을 만들어주는 빌더
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+        String itemDetail = "테스트 상품 상세 설명";
+        int price = 10003;
+        String itemSellStat = "SELL";
+        
+        booleanBuilder.and(item.itemDetail.like("%"+itemDetail+"%"));
+        booleanBuilder.and(item.price.gt(price));
+
+        // 상품을 조회하는데 필요한 and조건을 추가 -> 상품의 판매 상태가 SELL일 때만 booleanBuilder에 판매상태 조건을 동적으로 추가
+        if(StringUtils.equals(itemSellStat, ItemSellStatus.SELL)){
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+        
+        // 데이터를 페이징해 조회하도록 PageRequest.of(조회할 페이지의 번호, 한 페이지당 조회할 데이터의 개수)를 이용해 Pageble 객체를 생성
+        Pageable pageable = PageRequest.of(0,5);
+        
+        // QueryDslPredicateExecutor 인터페이스에서 정의한 findAll()를 이용해 조건에 맞는 데이터를 Page 객체로 받아옴
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println("total elements : "+ itemPagingResult.getTotalElements());
+
+        List<Item> resultItemList = itemPagingResult.getContent();
+        for(Item resultItem : resultItemList){
+            System.out.println(resultItem.toString());
+        }
+    }
+
 }
