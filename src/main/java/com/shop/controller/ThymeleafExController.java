8578051b63c0183e04887d48f2f826ceb035a004
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-09-14 20:41:35 +0900

    thymeleaf th:href

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -71,4 +71,9 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx04";
     }
 
+    @GetMapping(value = "/ex05")
+    public String thymeleafExample05(){
+        return "thymeleafEx/thymeleafEx05";
+    }
+
 }
