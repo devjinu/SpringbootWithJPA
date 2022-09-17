e9de9ca0101341e640c58cea18b9befc60b6434a
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-09-18 01:52:20 +0900

    auditing을 이용한 엔티티 공통 속성 공통화(시간)

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -30,8 +30,6 @@ public class OrderItem {
     // 주문 수량
     private int count;
 
-    private LocalDateTime regTime;
 
-    private LocalDateTime updateTime;
 
 }
