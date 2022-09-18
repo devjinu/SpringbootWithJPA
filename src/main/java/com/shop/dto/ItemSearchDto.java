0d9bab40e936f2186bd974a56c23f16433936903
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-09-19 01:05:05 +0900

    상품 관리하기

---
+++ src/main/java/com/shop/dto/ItemSearchDto.java
@@ -0,0 +1,21 @@
+package com.shop.dto;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemSearchDto {
+
+    private String searchDateType;
+
+    private ItemSellStatus searchSellStatus;
+
+    // itemNm : 상품명
+    // createdBy : 상품등록자 아이디
+    private String searchBy;
+    
+    private String searchQuery = "";
+    
+}
