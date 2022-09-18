22ba49ebfd93021d8c980cb5e4bc282df563bc39
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-09-18 22:11:03 +0900

    상품 수정 완료

--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -37,5 +39,24 @@ public class ItemImgService {
         //상품 이미지 정보 저장
         itemImg.updateItemImg(oriImgName, imgName, imgUrl);
         itemImgRepository.save(itemImg);
+    }
+
+    public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception{
+        // 상품 이미지를 수정한 경우 이미지를 업데이트
+        if(!itemImgFile.isEmpty()){
+            // 상품 이미지 아이디를 이용하여 기존에 저장했던 상품 이미지 엔티티를 조회
+            ItemImg savedItemImg = itemImgRepository.findById(itemImgId).orElseThrow(EntityNotFoundException::new);
+            // 기존에 등록된 상품 이미지 파일이 있을 경우 해당 파일을 삭제
+            if(!StringUtils.isEmpty(savedItemImg.getImgName())){
+                fileService.deleteFile(itemImgLocation+"/"+savedItemImg.getImgName());
+            }
+
+            String oriImgName = itemImgFile.getOriginalFilename();
+            // 업데이트한 상품 이미지 파일을 업로드
+            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes()) ;
+            String imgUrl = "/images/item/" + imgName;
+            // 변경된 상품 이미지 정보를 세팅
+            savedItemImg.updateItemImg(oriImgName,imgName,imgUrl);
+        }
     }
 }
--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -46,4 +49,36 @@ public class ItemService {
         return item.getId();
     }
 
+    // 1. 해당 상품의 이미지 조회 -> 등록순으로 조회하기 위해 오름차순
+    // 2. 조회한 ItemImg 엔티티를 ItemImgDto 객체로 만들어서 리스트에 추가
+    // 3. 상품의 아이디를 통해 상품 엔티티 조회
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId) {
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
+    }
+
+    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
+        // 상품 수정
+        // 1. 상품 등록화면으로 부터 전달 받은 상품 아이디를 이용하여 상품 엔티티를 조회
+        Item item = itemRepository.findById(itemFormDto.getId()).orElseThrow(EntityNotFoundException::new);
+        // 2. 상품 등록화면으로부터 전달 받은 ItemFormDto를 통해 상품 엔티티를 업데이트
+        item.updateItem(itemFormDto);
+        // 3. 상품 이미지 아이디 리스트를 조회 
+        List<Long> itemImgIds = itemFormDto.getItemImgIds();
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            // 4. 상품 이미지를 업데이트 하기 위해서 메소드에 상품이미지 아이디와 성품이미지 파일정보를 파라미터로 전달
+            itemImgService.updateItemImg(itemImgIds.get(i),itemImgFileList.get(i));
+
+        }
+        return item.getId();
+    }
 }
