7d00d73c93b56b80a33d990b52286c2d651520c8
moonjinu <69113782+moonjinu@users.noreply.github.com>
2022-09-19 23:29:21 +0900

    메인 페이지 추가 및 에러 수정

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,7 +1,6 @@
 package com.shop.controller;
 
 import com.shop.dto.ItemDto;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
